45a7ba21edd2975d4084708c971fc03f880fcd2f
Zahed <zahedm45@gmail.com>
2025-01-22 14:59:44 +0100

    ...

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -21,7 +21,11 @@ void matmult_mkn_offload(int m, int n, int k, double **A, double **B, double **C
             C[i][j] = 0;
         }
     }
+// Minimum number of threads for full throughput (this is not optimal!): N SMs x 4 x 32
 
+
+// Minimum number of threads for full occupancy (this is often optimal): N SMs x 4 x 12 x 32 =
+// If there is an imbalanced amount of work, use one thread per iteration.
 #pragma omp target teams distribute parallel for num_teams(114) thread_limit(64) \
     map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
     for (int i = 0; i < m; i++) {
@@ -42,6 +46,7 @@ void matmult_mnk_offload(int m, int n, int k, double **A, double **B, double **C
 #pragma omp target teams distribute parallel for num_teams(114) thread_limit(64) \
         map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
     for (int i = 0; i < m; i++) {
+
         for (int j = 0; j < n; j++) {
             double sum = 0;
             for (int l = 0; l < k; l++) {

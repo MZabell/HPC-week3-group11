0c119e716ef3903f08ea0a8eec90e93a828c39b9
Zahed <zahedm45@gmail.com>
2025-01-22 19:29:48 +0100

    Temp

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -96,4 +96,36 @@ void matmult_mnk_offload(int m, int n, int k, double **A, double **B, double **C
 }
 
 
+
+
+
+
+
+
+/* ---------------- BLK -----------------------*/
+
+void matmult_blk_offload(int m, int n, int k, double **A, double **B, double **C) {
+    const int bs = 8;
+#pragma omp target teams distribute parallel for num_teams(114) thread_limit(64) \
+        map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
+    for (int i_block = 0; i_block < m; i_block += bs) {
+
+        for (int i = i_block; i < i_block + bs && i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                double sum = 0;
+                for (int l = 0; l < k; l++) {
+                    sum += A[i][l] * B[l][j];
+                }
+                C[i][j] = sum;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
 }

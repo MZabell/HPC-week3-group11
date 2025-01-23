f7a06e30b7568b33db8d8a88d3257eb33277967c
Zahed <zahedm45@gmail.com>
2025-01-23 12:02:08 +0100

    mxn 128 128

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -39,6 +39,9 @@ void matmult_mkn_offload_min(int m, int n, int k, double **A, double **B, double
 }
 
 
+
+
+
 void matmult_mkn_offload(int m, int n, int k, double **A, double **B, double **C) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -46,7 +49,10 @@ void matmult_mkn_offload(int m, int n, int k, double **A, double **B, double **C
         }
     }
 
-#pragma omp target teams distribute parallel for collapse(2) num_teams(114) thread_limit(64) \
+    int thread_size = 128;
+    int team_size = m * n / thread_size;
+
+#pragma omp target teams distribute parallel for collapse(2) num_teams(team_size) thread_limit(thread_size) \
         map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {

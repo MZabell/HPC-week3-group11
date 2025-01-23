fde9657d3f4f01b4b6d1299a32c5897f8482c1d8
Zahed <zahedm45@gmail.com>
2025-01-23 13:41:15 +0100

    mnk cleanup

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -59,43 +59,6 @@ void matmult_mnk_offload(int m, int n, int k, double **A, double **B, double **C
 }
 
 
-void matmult_mnk_offload2(int m, int n, int k, double **A, double **B, double **C) {
-#pragma omp target teams distribute parallel for collapse(2) \
-    num_teams(114) thread_limit(64) \
-    map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            double sum = 0;
-            for (int l = 0; l < k; l++) {
-                sum += A[i][l] * B[l][j];
-            }
-            C[i][j] = sum;
-        }
-    }
-}
-
-
-void matmult_mnk_offload_4(int m, int n, int k, double **A, double **B, double **C) {
-
-#pragma omp target teams distribute parallel for collapse(2) \
-    map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-
-            if (i == 0 && j == 0) {
-                printf("Number of teams: %d\n", omp_get_num_teams());
-                printf("Number of thrads: %d\n", omp_get_num_threads());
-            }
-
-            double sum = 0;
-            for (int l = 0; l < k; l++) {
-                sum += A[i][l] * B[l][j];
-            }
-            C[i][j] = sum;
-        }
-    }
-}
-
 
 /* ---------------- BLK -----------------------*/
 

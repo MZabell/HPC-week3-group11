00e88213989376c69adcd161072c59ad5f06f507
Zahed <zahedm45@gmail.com>
2025-01-23 12:44:00 +0100

    Cleanup

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -20,29 +20,9 @@ extern "C" {
 #include <stdio.h>
 
 
-void matmult_mkn_offload_min(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            C[i][j] = 0;
-        }
-    }
-#pragma omp target teams distribute parallel for num_teams(114) thread_limit(64) \
-    map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
-    for (int i = 0; i < m; i++) {
-        // You want adjacent threads to access adjacent memory. E.g. thread 1,2,3,... access outermost index 1,2,3...
-        for (int l = 0; l < k; l++) {
-            for (int j = 0; j < n; j++) {
-                C[i][j] += A[i][l] * B[l][j];
-            }
-        }
-    }
-}
 
 
-
-
-
-void matmult_mkn_offload(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
+void matmult_mkn_offload_main(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             C[i][j] = 0;
@@ -65,31 +45,30 @@ void matmult_mkn_offload(int m, int n, int k, const double **A, const double **B
 }
 
 
-
-
-void matmult_mkn_offload_ignore(int m, int n, int k, double **A, double **B, double **C) {
+void matmult_mkn_offload(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             C[i][j] = 0;
         }
     }
-// Minimum number of threads for full throughput (this is not optimal!): N SMs x 4 x 32
 
+    //int thread_size = 128;
+    int thread_size = 192;
+    int team_size = m * n / thread_size;
 
-// Minimum number of threads for full occupancy (this is often optimal): N SMs x 4 x 12 x 32 =
-// If there is an imbalanced amount of work, use one thread per iteration.
-#pragma omp target teams distribute parallel for collapse(2) num_teams(64)  \
-    map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
+#pragma omp target teams distribute parallel for collapse(2) num_teams(team_size) thread_limit(thread_size) \
+        map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
     for (int i = 0; i < m; i++) {
-        for (int l = 0; l < k; l++) {
-            for (int j = 0; j < n; j++) {
-                C[i][j] += A[i][l] * B[l][j];
+        for (int j = 0; j < n; j++) {
+            double sum = 0;
+            for (int l = 0; l < k; l++) {
+                sum += A[i][l] * B[l][j];
             }
+            C[i][j] = sum;
         }
     }
-
-    //printf("%f\n", C[3][3]);
 }
+
 
 
 /* ---------------- MNK -----------------------*/

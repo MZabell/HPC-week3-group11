0bb5f2a2742c00adf61880e0d9cbc4c41dc821ec
Zahed <zahedm45@gmail.com>
2025-01-23 12:46:02 +0100

    Cleanup (mkn)

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -20,38 +20,7 @@ extern "C" {
 #include <stdio.h>
 
 
-
-
-void matmult_mkn_offload_main(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            C[i][j] = 0;
-        }
-    }
-
-    //int thread_size = 128;
-    int thread_size = 192;
-    int team_size = m * n / thread_size;
-
-#pragma omp target teams distribute parallel for collapse(2) num_teams(team_size) thread_limit(thread_size) \
-        map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int l = 0; l < k; l++) {
-                C[i][j] += A[i][l] * B[l][j];
-            }
-        }
-    }
-}
-
-
 void matmult_mkn_offload(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            C[i][j] = 0;
-        }
-    }
-
     //int thread_size = 128;
     int thread_size = 192;
     int team_size = m * n / thread_size;

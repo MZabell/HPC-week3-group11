544ddba4ca7926ee7edfeba4481bb26024a52c58
Zahed <zahedm45@gmail.com>
2025-01-23 15:11:35 +0100

    TA provided mnk

--- 02614_Assignment3_matmult/matmult_funs.cpp
+++ 02614_Assignment3_matmult/matmult_funs.cpp
@@ -66,13 +66,32 @@ void matmult_mnk_offload_main(int m, int n, int k, const double **A, const doubl
 }
 
 
-void matmult_mnk_offload(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
+void matmult_mnk_offload2(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
     int thread_size = 128;
     int team_size = m * n / thread_size;
 
 #pragma omp target teams distribute parallel for collapse(2) num_teams(team_size) thread_limit(thread_size) \
         map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
     for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            double sum = 0;
+            for (int l = 0; l < k; l++) {
+                sum += A[i][l] * B[l][j];
+            }
+            C[i][j] = sum;
+        }
+    }
+}
+
+
+void matmult_mnk_offload(int m, int n, int k, const double **A, const double **B, double **__restrict__ C) {
+    int thread_size = 128;
+    int team_size = m * n / thread_size;
+
+#pragma omp target teams distribute num_teams(team_size) thread_limit(thread_size) \
+        map(to: A[0:m][0:k], B[0:k][0:n]) map(tofrom: C[0:m][0:n])
+    for (int i = 0; i < m; i++) {
+    #pragma omp parallel for
         for (int j = 0; j < n; j++) {
             double sum = 0;
             for (int l = 0; l < k; l++) {
